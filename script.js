e9ae61278fba7778d8e5c852fa4aa9d3432d3244
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-04 16:36:27 +0100

    Add basic variable declaring and logging

--- script.js
+++ script.js
@@ -3,3 +3,14 @@
 console.log(23 + 97); // 120
 console.log(43 + 23 + 9 + 102 + 18 + 71); // 266
 console.log((4 + 6 + 9) / 77); // 0.24675324675324675
+
+// Declaring and Logging Variables
+
+let a = 10;
+console.log(a); // 10
+
+a = 15; // Reassign a to 15
+console.log(a); // 15
+
+let b = 7 * a;
+console.log(b); // 105 (7 x reassigned value of a : 7 x 15 = 105)
