8339deda00ae1b44ded38dad312d5317eafe5c56
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-04 16:56:47 +0100

    Add explanations of operations in javascript

--- script.js
+++ script.js
@@ -22,3 +22,33 @@ const actual = max - 13;
 const percentage = actual / max;
 
 console.log(percentage); // 0.7719298245614035
+
+// Operations
+
+// Addition +
+
+console.log(15 + 5); // 20
+
+// Subtraction -
+
+console.log(20 - 5); // 15
+
+// Multiplication *
+
+console.log(10 * 10); // 100
+
+// Division /
+
+console.log(100 / 4); // 25
+
+// Remainder % (Remainder of the division)
+
+console.log(5 % 2); // 1 (2 only fits 2 times and leaves 1)
+console.log(8 % 3); // 2 (3 only fits 2 times and leaves 2)
+console.log(8 % 4); // 0 (4 fits twice and leaves 0)
+
+// Exponentiation ** (to the power of)
+
+console.log(2 ** 2); // 4 (2 x 2 = 4)
+console.log(2 ** 3); // 8 (2 x 2 x 2 = 8)
+console.log(2 ** 4); // 16 (2 x 2 x 2 x 2 = 16)
