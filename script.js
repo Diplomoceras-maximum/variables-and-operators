f13c3dcc4d469488be54bcbe412d98e9e894dfd8
Max <214122627+Diplomoceras-maximum@users.noreply.github.com>
2025-06-04 16:30:41 +0100

    Add answer comments to basic maths console logs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Logging Basic Maths
 
-console.log(23 + 97);
-console.log(43 + 23 + 9 + 102 + 18 + 71);
-console.log((4 + 6 + 9) / 77);
+console.log(23 + 97); // 120
+console.log(43 + 23 + 9 + 102 + 18 + 71); // 266
+console.log((4 + 6 + 9) / 77); // 0.24675324675324675
